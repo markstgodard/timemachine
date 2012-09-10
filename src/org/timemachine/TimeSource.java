f489a9db4be8fc3636e1c7e0ba893e57b48027bb
Mark St.Godard <markstgodard@gmail.com>
2012-09-09 22:20:34 -0500

    Add test dir, cleanup

--- src/org/timemachine/TimeSource.java
+++ src/org/timemachine/TimeSource.java
@@ -3,18 +3,19 @@ package org.timemachine;
 import java.sql.Timestamp;
 
 /**
- * {@link TimeSource} defines how time is derived 
- * (i.e. from {@link System} currentTimeMillis or otherwise).
+ * {@link TimeSource} defines how time is derived (i.e. from {@link System}
+ * currentTimeMillis or otherwise).
  * 
  * @author markstgodard@gmail.com
- *
+ * 
  */
 public interface TimeSource {
 
-	/**
-	 * Represents the current (or faked) time.
-	 * @return
-	 */
-	public Timestamp now();
-	
+  /**
+   * Represents the current (or faked) time.
+   * 
+   * @return The current time as timestamp
+   */
+  public Timestamp now();
+
 }
